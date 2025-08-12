b0309d6a6d0f4a9a03ab0f225ce8eb94577306b5
RAQUEL_SANTOS <raquel.santosfgv@gmail.com>
2025-08-12 15:49:52 -0300

    sorteio e média

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-print ("Hello World")
+print ("Hello World")]
+
+import random 
 
 def cumprimento (texto):
     return f"Olá {texto}"
+
+def media_7_numeros():
+    numeros = [random.randint(1, 100) for _ in range(7)]
+    media = sum(numeros) / len(numeros)
+    return numeros, media
+
+
 if __name__ == "__main__":
     nome_completo = "Raquel Santos"
     print (cumprimento(nome_completo))
+    numeros, media = media_7_numeros()
+    print(f"Números sorteados: {numeros}")
+    print(f"Média dos números: {media}")
+
